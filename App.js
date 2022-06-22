8fdb834cb7507e96a8aa3e5e5a092ab22bca5878
Norman <norman_cade32@yahoo.com>
2022-06-22 15:53:59 -0400

    init files

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+// Loading fonts
+function fetchFonts() {
+  return Font.loadAsync({
+    poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+    "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "poppins-medium": require("./assets/fonts/Poppins-Medium.ttf"),
+  });
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      // Make sure all fonts are loaded before app shows screen
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/constants/Colors.js
@@ -0,0 +1,6 @@
+export default {
+  darkGray: "#b8c6cd",
+  textColor: "#0E2525",
+  darkGreen: "#4C5A5A",
+  lightGreen: "#78888A",
+};
