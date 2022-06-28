9d46ac52677d4867dbaa75e1bdb861428cf27b0c
Norman <norman_cade32@yahoo.com>
2022-06-28 19:23:29 -0400

    add note field to add chore screen

--- src/screens/AddChoreScreen/AddChoreScreen.js
+++ src/screens/AddChoreScreen/AddChoreScreen.js
@@ -6,6 +6,8 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
+  Platform,
+  KeyboardAvoidingView,
   FlatList,
 } from "react-native";
 import React from "react";
@@ -99,48 +101,60 @@ const AddChoreScreen = () => {
       resizeMode="cover"
     >
       <SafeAreaView>
-        <View style={styles.screenContainer}>
-          <View style={{ position: "absolute", left: 25 }}>
-            <Feather name="arrow-left" size={35} color={Colors.darkGreen} />
-          </View>
-          <Text style={styles.addChoreTitle}>Add chore</Text>
-          <View style={styles.contentContainer}>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputTitle}>Chore</Text>
-              <TextInput
-                style={styles.inputField}
-                placeholder={"Type chore here"}
-              />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.container}
+        >
+          <View style={styles.screenContainer}>
+            <View style={{ position: "absolute", left: 25 }}>
+              <Feather name="arrow-left" size={35} color={Colors.darkGreen} />
             </View>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputTitle}>Points</Text>
-              <TextInput
-                returnKeyType="done"
-                keyboardType="numeric"
-                style={styles.inputField}
-                placeholder={"Give chore points"}
-              />
-            </View>
-            <View style={[styles.inputContainer, { marginBottom: 0 }]}>
-              <Text style={styles.inputTitle}>Icon</Text>
-            </View>
-            <View style={{ marginBottom: 30 }}>
-              <FlatList
-                showsHorizontalScrollIndicator={false}
-                horizontal={true}
-                contentContainerStyle={styles.choreIconListContainer}
-                data={CHORESICONS}
-                renderItem={({ item }) => (
-                  <View style={styles.choreIconContainer}>{item}</View>
-                )}
-              />
-            </View>
+            <Text style={styles.addChoreTitle}>Add chore</Text>
+            <View style={styles.contentContainer}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputTitle}>Chore</Text>
+                <TextInput
+                  style={styles.inputField}
+                  placeholder={"Type chore here"}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputTitle}>Points</Text>
+                <TextInput
+                  returnKeyType="done"
+                  keyboardType="numeric"
+                  style={styles.inputField}
+                  placeholder={"Give chore points"}
+                />
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.inputTitle}>Note</Text>
+                <TextInput
+                  style={styles.inputField}
+                  placeholder={"Add additional information"}
+                />
+              </View>
+              <View style={[styles.inputContainer, { marginBottom: 0 }]}>
+                <Text style={styles.inputTitle}>Icon</Text>
+              </View>
+              <View style={{ marginBottom: 30 }}>
+                <FlatList
+                  showsHorizontalScrollIndicator={false}
+                  horizontal={true}
+                  contentContainerStyle={styles.choreIconListContainer}
+                  data={CHORESICONS}
+                  renderItem={({ item }) => (
+                    <View style={styles.choreIconContainer}>{item}</View>
+                  )}
+                />
+              </View>
 
-            <View style={styles.addChoreButtonContainer}>
-              <Text style={styles.addChoreText}>Create Chore</Text>
+              <View style={styles.addChoreButtonContainer}>
+                <Text style={styles.addChoreText}>Create Chore</Text>
+              </View>
             </View>
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </ImageBackground>
   );
