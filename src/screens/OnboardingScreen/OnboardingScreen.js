9dfeec04dc23b671f3e2424e16bf418367b51f84
Norman <norman_cade32@yahoo.com>
2022-07-06 19:59:18 -0400

    Can choose profile pic. Implement saving pic

--- src/screens/OnboardingScreen/OnboardingScreen.js
+++ src/screens/OnboardingScreen/OnboardingScreen.js
@@ -1,30 +1,140 @@
-import { View, Text } from "react-native";
-import { useEffect } from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  SafeAreaView,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { useEffect, useState } from "react";
 import React from "react";
 import styles from "./styles";
 import * as ImagePicker from "expo-image-picker";
+import BgImage from "../../../assets/images/familyMemberScreenBgImage.png";
+import { FontAwesome5, MaterialIcons, Entypo } from "@expo/vector-icons";
+import Colors from "../../constants/Colors";
+import SelectDropdown from "react-native-select-dropdown";
+import { useNavigation } from "@react-navigation/native";
 
 const OnboardingScreen = () => {
   const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
-  requestPermission();
-  useEffect(() => {
-    // async function getImagePermission() {
-    //   const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
-    //   if (status !== "granted") {
-    //     alert(
-    //       "Please grant camera roll permissions inside your system's settings"
-    //     );
-    //   } else {
-    //     console.log("Media Permissions are granted");
-    //   }
-    // }
-    // getImagePermission();
-  }, []);
+  const [image, setImage] = useState(null);
+  const navigation = useNavigation();
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  // useEffect(() => {
+  //   async function getMediaPermission() {
+  //     requestPermission();
+  //     if (status?.status !== "granted") {
+  //       alert(
+  //         "Please grant camera roll permissions inside your system's settings"
+  //       );
+  //     } else {
+  //       console.log("Media Permissions are granted");
+  //     }
+  //   }
+  //   getMediaPermission();
+  // }, []);
 
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <ImageBackground style={{ flex: 1 }} source={BgImage} resizeMode="cover">
+      <SafeAreaView style={{ justifyContent: "center" }}>
+        <View style={styles.profileInfoFormContainer}>
+          <Text style={styles.profileInfoTitle}>Profile Information</Text>
+          <View style={styles.profilePicContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                source={{ uri: image }}
+                style={{ width: 100, height: 100 }}
+              />
+              {/* <View style={{ top: 10 }}>
+                <FontAwesome5
+                  name="user-alt"
+                  size={80}
+                  color={Colors.lightGray}
+                />
+              </View> */}
+            </View>
+            <Pressable
+              onPress={pickImage}
+              style={styles.changeProfilePicButton}
+            >
+              <MaterialIcons name="edit" size={24} color={Colors.lightGray} />
+            </Pressable>
+          </View>
+          <View style={styles.textInputsContainer}>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputTitle}>First name</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder={" Type first name here"}
+                placeholderTextColor={Colors.darkGray}
+                selectionColor={Colors.lightGray}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputTitle}>Last name</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder={" Type last name here"}
+                placeholderTextColor={Colors.darkGray}
+                selectionColor={Colors.lightGray}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputTitle}>Gender</Text>
+              <SelectDropdown
+                dropdownOverlayColor={"Transparent"}
+                defaultButtonText={"Select a gender"}
+                renderDropdownIcon={() => (
+                  <Entypo name="chevron-small-down" size={24} color="white" />
+                )}
+                buttonTextStyle={styles.dropdownInput}
+                buttonStyle={styles.dropDown}
+                data={["Male", "Female", "Other"]}
+                onSelect={(selectedItem) => {
+                  console.log(selectedItem);
+                }}
+                rowStyle={{
+                  backgroundColor: Colors.darkGreen,
+                }}
+                rowTextStyle={styles.dropdownInput}
+                selectedRowStyle={{ backgroundColor: Colors.lightGreen }}
+              />
+            </View>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("HomeScreen")}
+            style={styles.addChoreButtonContainer}
+          >
+            <Text style={styles.addChoreText}>Done</Text>
+            <MaterialIcons
+              style={{ marginLeft: "auto", marginRight: 20 }}
+              name="arrow-forward-ios"
+              size={24}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
--- src/screens/OnboardingScreen/styles.js
+++ src/screens/OnboardingScreen/styles.js
@@ -1,4 +1,113 @@
 import { StyleSheet } from "react-native";
 import Colors from "../../constants/Colors";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  profileInfoFormContainer: {
+    height: "90%",
+    marginHorizontal: 25,
+    borderRadius: 15,
+    backgroundColor: Colors.darkGreen,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    paddingTop: 25,
+  },
+  profileInfoTitle: {
+    color: "white",
+    fontFamily: "poppins-medium",
+    fontSize: 18,
+    marginBottom: 25,
+  },
+  profilePicContainer: {
+    flexDirection: "row",
+    marginBottom: 25,
+  },
+  imageContainer: {
+    backgroundColor: Colors.darkGray,
+    height: 100,
+    width: 100,
+    borderRadius: 100 / 2,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  changeProfilePicButton: {
+    backgroundColor: Colors.lightGreen,
+    width: 30,
+    height: 30,
+    borderRadius: 30 / 2,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+    left: 70,
+  },
+  textInputsContainer: {
+    width: "100%",
+    // flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 50,
+    marginBottom: 30,
+  },
+  inputTitle: {
+    fontFamily: "poppins",
+    color: "white",
+    marginBottom: 5,
+  },
+  inputField: {
+    color: "white",
+    width: "100%",
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    marginTop: 5,
+    fontSize: 16,
+    fontFamily: "poppins",
+    paddingBottom: 5,
+  },
+  dropdownInput: {
+    textAlign: "left",
+    fontFamily: "poppins",
+    fontSize: 16,
+    color: "white",
+  },
+  dropDown: {
+    backgroundColor: "transparent",
+    height: 35,
+    width: "100%",
+  },
+  addChoreButtonContainer: {
+    backgroundColor: Colors.lightGreen,
+    flexDirection: "row",
+    width: "80%",
+    // marginTop: 20,
+    marginLeft: "auto",
+    marginTop: "auto",
+    marginBottom: 65,
+    padding: 15,
+    borderBottomLeftRadius: 15,
+    borderTopLeftRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  addChoreText: {
+    color: "white",
+    fontFamily: "poppins-medium",
+    fontSize: 16,
+    marginLeft: 25,
+  },
+});
